92c90e46c3cb8e6bed4d0ec3221762a5c2db106e
Leo Zhang <86931572+leoz0214@users.noreply.github.com>
2022-08-05 00:04:22 +0100

    added more elements

--- src/element.py
+++ src/element.py
@@ -30,6 +30,7 @@ class Element:
         # are all supported.
         # Density is in g/cm^3
         match info:
+
             case "hydrogen" | "h" | 1:
                 self.name = "hydrogen"
                 self.symbol = "H"
@@ -43,8 +44,94 @@ class Element:
                 self.boiling_point_k = 20.271
                 self.density = 0.00008988
                 self.stable = True
-                self.discoverer = "Henry Cavendish"
+                self.discovery = "Henry Cavendish"
                 self.discovery_year = 1766
+
+            case "helium" | "he" | 2:
+                self.name = "helium"
+                self.symbol = "He"
+                self.atomic_number = 2
+                self.atomic_mass = 4.0026
+                self.electrons_per_shell = (2,)
+                self.state = "gas"
+                self.group = 18
+                self.period = 1
+                self.melting_point_k = 0.95
+                self.boiling_point_k = 4.222
+                self.density = 0.0001786
+                self.stable = True
+                self.discovery = "Pierre Janssen, Norman Lockyer"
+                self.discovery_year = 1868
+
+            case "lithium" | "li" | 3:
+                self.name = "lithium"
+                self.symbol = "Li"
+                self.atomic_number = 3
+                self.atomic_mass = 6.94
+                self.electrons_per_shell = (2, 1)
+                self.state = "solid"
+                self.group = 1
+                self.period = 2
+                self.melting_point_k = 453.65
+                self.boiling_point_k = 1603
+                self.density = 0.534
+                self.stable = True
+                self.discovery = "Johan August Arfwedson"
+                self.discovery_year = 1817
+            
+            case "beryllium" | "be" | 4:
+                self.name = "beryllium"
+                self.symbol = "Be"
+                self.atomic_number = 4
+                self.atomic_mass = 9.0122
+                self.electrons_per_shell = (2, 2)
+                self.state = "solid"
+                self.group = 2
+                self.period = 2
+                self.melting_point_k = 1560
+                self.boiling_point_k = 2742
+                self.density = 1.85
+                self.stable = True
+                self.discovery = "Louis Nicolas Vauquelin"
+                self.discovery_year = 1798
+            
+            case "boron" | "b" | 5:
+                self.name = "boron"
+                self.symbol = "B"
+                self.atomic_number = 5
+                self.atomic_mass = 10.81
+                self.electrons_per_shell = (2, 3)
+                self.state = "solid"
+                self.group = 13
+                self.period = 2
+                self.melting_point_k = 2349
+                self.boiling_point_k = 4200
+                self.density = 2.34
+                self.stable = True
+                self.discovery = (
+                    "Joseph Louis Gay-Lussac, Louis Jacques Thénard")
+                self.discovery_year = 1808
+            
+            case "carbon" | "c" | 6:
+                self.name = "carbon"
+                self.symbol = "C"
+                self.atomic_number = 6
+                self.atomic_mass = 12.011
+                self.electrons_per_shell = (2, 4)
+                self.state = "solid"
+                self.group = 14
+                self.period = 2
+                self.melting_point_k = 3823
+                self.boiling_point_k = 5100
+                self.density = 2.27
+                self.stable = True
+                self.discovery = "Antoine Lavoisier"
+                self.discovery_year = 1789
+
+
+
+        
+
             case _:
                 raise ValueError(
                     ("Info does not match the name, symbol or atomic number "
@@ -90,8 +177,12 @@ class Element:
         return self.atomic_number
 
     def __repr__(self) -> str:
+        """
+        Returns available element information as one string.
+        Useful for outputting element data to the console.
+        """
         lines = [
-            f"Name: {self.name}",
+            f"Name: {self.name.title()}",
             f"Symbol: {self.symbol}",
             f"Atomic number: {self.atomic_number}",
             f"Atomic mass: {self.atomic_mass}"
@@ -101,7 +192,7 @@ class Element:
             lines.append(f"Electrons per shell: {self.electrons_per_shell}")
 
         if self.state is not None:
-            lines.append(f"State (room temperature): {self.state}")
+            lines.append(f"State (room temperature): {self.state.title()}")
 
         lines.append(f"Group: {self.group}")
         lines.append(f"Period: {self.period}")
@@ -115,18 +206,13 @@ class Element:
         if self.density is not None:
             lines.append(f"Density (room temperature): {self.density} g/cm³")
         
-        lines.append(f"Is stable: {self.stable}")
+        lines.append(f"Has stable isotope(s): {self.stable}")
         lines.append(
-            f"Discovered by: {self.discoverer} in {self.discovery_year}"
+            f"Discovered by: {self.discovery} in {self.discovery_year}"
         )
 
         return "\n".join(lines)
 
 
-
-
-        
-    
-
 if __name__ == "__main__":
-    print(Element("h"))
+    print(Element(6))
