8c2f3ac6d10d342facde8e6d9613a3b10912819c
Leo Zhang <86931572+leoz0214@users.noreply.github.com>
2022-08-05 09:07:22 +0100

    added more elements

--- src/element.py
+++ src/element.py
@@ -43,7 +43,7 @@ class Element:
                 self.melting_point_k = 13.99
                 self.boiling_point_k = 20.271
                 self.density = 0.00008988
-                self.stable = True
+                self.has_stable_isotope = True
                 self.discovery = "Henry Cavendish"
                 self.discovery_year = 1766
 
@@ -59,7 +59,7 @@ class Element:
                 self.melting_point_k = 0.95
                 self.boiling_point_k = 4.222
                 self.density = 0.0001786
-                self.stable = True
+                self.has_stable_isotope = True
                 self.discovery = "Pierre Janssen, Norman Lockyer"
                 self.discovery_year = 1868
 
@@ -75,7 +75,7 @@ class Element:
                 self.melting_point_k = 453.65
                 self.boiling_point_k = 1603
                 self.density = 0.534
-                self.stable = True
+                self.has_stable_isotope = True
                 self.discovery = "Johan August Arfwedson"
                 self.discovery_year = 1817
             
@@ -91,7 +91,7 @@ class Element:
                 self.melting_point_k = 1560
                 self.boiling_point_k = 2742
                 self.density = 1.85
-                self.stable = True
+                self.has_stable_isotope = True
                 self.discovery = "Louis Nicolas Vauquelin"
                 self.discovery_year = 1798
             
@@ -107,7 +107,7 @@ class Element:
                 self.melting_point_k = 2349
                 self.boiling_point_k = 4200
                 self.density = 2.34
-                self.stable = True
+                self.has_stable_isotope = True
                 self.discovery = (
                     "Joseph Louis Gay-Lussac, Louis Jacques Thénard")
                 self.discovery_year = 1808
@@ -124,13 +124,96 @@ class Element:
                 self.melting_point_k = 3823
                 self.boiling_point_k = 5100
                 self.density = 2.27
-                self.stable = True
+                self.has_stable_isotope = True
                 self.discovery = "Antoine Lavoisier"
                 self.discovery_year = 1789
+            
+            case "nitrogen" | "n" | 7:
+                self.name = "nitrogen"
+                self.symbol = "N"
+                self.atomic_number = 7
+                self.atomic_mass = 14.007
+                self.electrons_per_shell = (2, 5)
+                self.state = "gas"
+                self.group = 15
+                self.period = 2
+                self.melting_point_k = 63.23
+                self.boiling_point_k = 77.355
+                self.density = 0.00125
+                self.has_stable_isotope = True
+                self.discovery = "Daniel Rutherford"
+                self.discovery_year = 1772
+            
+            case "oxygen" | "o" | 8:
+                self.name = "oxygen"
+                self.symbol = "O"
+                self.atomic_number = 8
+                self.atomic_mass = 15.999
+                self.electrons_per_shell = (2, 6)
+                self.state = "gas"
+                self.group = 16
+                self.period = 2
+                self.melting_point_k = 54.36
+                self.boiling_point_k = 90.188
+                self.density = 0.001429
+                self.has_stable_isotope = True
+                self.discovery = "Carl Wilhelm Scheele"
+                self.discovery_year = 1771
+            
+            case "fluorine" | "f" | 9:
+                self.name = "fluorine"
+                self.symbol = "F"
+                self.atomic_number = 9
+                self.atomic_mass = 18.998
+                self.electrons_per_shell = (2, 7)
+                self.state = "gas"
+                self.group = 17
+                self.period = 2
+                self.melting_point_k = 53.48
+                self.boiling_point_k = 85.03
+                self.density = 0.001696
+                self.has_stable_isotope = True
+                self.discovery = "André-Marie Ampère"
+                self.discovery_year = 1810
+            
+            case "neon" | "ne" | 10:
+                self.name = "neon"
+                self.symbol = "Ne"
+                self.atomic_number = 10
+                self.atomic_mass = 20.18
+                self.electrons_per_shell = (2, 8)
+                self.state = "gas"
+                self.group = 18
+                self.period = 2
+                self.melting_point_k = 24.56
+                self.boiling_point_k = 27.104
+                self.density = 0.0009002
+                self.has_stable_isotope = True
+                self.discovery = "William Ramsay, Morris Travers"
+                self.discovery_year = 1898
+            
+            case "sodium" | "na" | 11:
+                self.name = "sodium"
+                self.symbol = "Na"
+                self.atomic_number = 11
+                self.atomic_mass = 22.99
+                self.electrons_per_shell = (2, 8, 1)
+                self.state = "solid"
+                self.group = 1
+                self.period = 3
+                self.melting_point_k = 370.944
+                self.boiling_point_k = 1156.09
+                self.density = 0.968
+                self.has_stable_isotope = True
+                self.discovery = "Humphry Davy"
+                self.discovery_year = 1807 
+            
+            
 
 
+             
 
-        
+
 
             case _:
                 raise ValueError(
@@ -198,15 +281,27 @@ class Element:
         lines.append(f"Period: {self.period}")
 
         if self.melting_point_k is not None:
-            lines.append(f"Melting point: {self.melting_point_k} K")
+            lines.append(
+                "Melting point: {} K / {} °C / {} °F".format(
+                    self.melting_point_k, 
+                    self.melting_point_c,
+                    self.melting_point_f
+                )
+            )
         
         if self.boiling_point_k is not None:
-            lines.append(f"Boiling point: {self.boiling_point_k} K")
+            lines.append(
+                "Melting point: {} K / {} °C / {} °F".format(
+                    self.boiling_point_k, 
+                    self.boiling_point_c,
+                    self.boiling_point_f
+                )
+            )
         
         if self.density is not None:
             lines.append(f"Density (room temperature): {self.density} g/cm³")
         
-        lines.append(f"Has stable isotope(s): {self.stable}")
+        lines.append(f"Has stable isotope(s): {self.has_stable_isotope}")
         lines.append(
             f"Discovered by: {self.discovery} in {self.discovery_year}"
         )
@@ -215,4 +310,4 @@ class Element:
 
 
 if __name__ == "__main__":
-    print(Element(6))
+    print(Element("SODIUM"))
