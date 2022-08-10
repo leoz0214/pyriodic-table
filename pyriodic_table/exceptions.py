b9b262dbcb31e93487968bd63a6a62660fd29189
Leo Zhang <86931572+leoz0214@users.noreply.github.com>
2022-08-10 11:45:34 +0100

    base exception: PyriodicTableException

--- pyriodic_table/exceptions.py
+++ pyriodic_table/exceptions.py
@@ -1,4 +1,10 @@
-class ElementDoesNotExist(Exception):
+class PyriodicTableException(Exception):
+    """
+    Base class for all pyriodic_table exceptions.
+    """
+    
+
+class ElementDoesNotExist(PyriodicTableException):
     """
     Chemical element does not exist (or remains undiscovered).
     """
